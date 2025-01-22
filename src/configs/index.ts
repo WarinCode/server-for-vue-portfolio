7c9d56882cf18c5145499721c8ed422a5e92ec28
WarinCode <warinsaipanya471@outlook.co.th>
2025-01-22 22:40:31 +0700

    Update cors origin handling for development mode

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -4,7 +4,7 @@ const isDevmode: boolean = process.env?.NODE_ENV?.trim() === "development";
 export const port: number = 3452;
 
 export const corsOptions: CorsOptions = {
-    origin: isDevmode ? `http://localhost:${port}` : "https://react-portfolio-lac-xi.vercel.app",
+    origin: isDevmode ? "*" : "https://react-portfolio-lac-xi.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }
