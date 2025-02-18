0d191888bf994e8ec31f6b7a8fe43d56ce6a8e0a
WarinCode <warinsaipanya471@outlook.co.th>
2025-02-18 23:41:56 +0700

    Fixed cors options

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -3,7 +3,7 @@ import { CorsOptions } from "cors";
 export const port: number = 3452;
 
 export const corsOptions: CorsOptions = {
-    origin: "https://vue-portfolio-beta-ochre.vercel.app/",
+    origin: "https://vue-portfolio-beta-ochre.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }
