a4afb9163f4c337558f304a4eb48515152b98002
WarinCode <warinsaipanya471@outlook.co.th>
2025-02-18 23:37:35 +0700

    Update cors option

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -3,7 +3,7 @@ import { CorsOptions } from "cors";
 export const port: number = 3452;
 
 export const corsOptions: CorsOptions = {
-    origin: ["*", "http://localhost:5173"],
+    origin: "https://vue-portfolio-beta-ochre.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }
