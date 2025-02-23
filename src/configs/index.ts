ea64a28c281c5abeb1db2086bedbeb168b89cef9
WarinCode <warinsaipanya471@outlook.co.th>
2025-02-23 21:32:50 +0700

    Update cors

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -3,7 +3,7 @@ import { CorsOptions } from "cors";
 export const port: number = 3452;
 
 export const corsOptions: CorsOptions = {
-    origin: process.env?.NODE_ENV === "production" ? "https://vue-portfolio-6kpu.onrender.com" : "*",
+    origin: process.env?.NODE_ENV === "production" ? "https://vue-portfolio-3593.netlify.app" : "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }
