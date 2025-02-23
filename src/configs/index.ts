df192a59165229a1a7ad47ed9a31909570e0cc9a
WarinCode <warinsaipanya471@outlook.co.th>
2025-02-23 20:51:41 +0700

    Update cors

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -3,7 +3,7 @@ import { CorsOptions } from "cors";
 export const port: number = 3452;
 
 export const corsOptions: CorsOptions = {
-    origin: process.env?.NODE_ENV === "production" ? "" : "*",
+    origin: process.env?.NODE_ENV === "production" ? "https://vue-portfolio-wine.vercel.app" : "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }
